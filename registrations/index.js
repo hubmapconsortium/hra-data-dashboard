6b8d39665eb28f91ce7fbba56f6bdfe4c6b5370c
Bruce W. Herr II <bherr@indiana.edu>
2022-03-21 16:09:13 -0400

    Added 'All Tissue Providers' provider

--- registrations/index.js
+++ registrations/index.js
@@ -1,6 +1,10 @@
 async function buildVega(graph) {
   const spec = await fetch("vis.vl.json").then((result) => result.json());
   spec.datasets.nodes = graph.nodes.map(n => n.data).filter(n => !!n.status_color);
+  spec.datasets.nodes = spec.datasets.nodes.concat(
+    spec.datasets.nodes.map(n => ({ ...n, provider: 'All Tissue Providers' }))
+  );
+
   return await vegaEmbed("#visualization", spec, { "renderer": "canvas", "actions": true });
 }
 
